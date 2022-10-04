fce994e9bf1be11f4c8cfee24a99cfae2cd04a90
Revolt-47 <i201806@nu.edu.pk>
2022-10-04 11:51:53 +0500

    update_app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import './index.css';
 import Form from './form.js'
 import Login from './login';
+import { useState } from 'react';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 
-function App() {
+function App(props) {
+
+  const [res,setres] = useState(null);
+
+  const callapi = () =>{
+    fetch('http://localhost:9000/index.js')
+    .then(res => res.text)
+    .then (res => setres(res));
+  }
+
+  function componentWillMount(){
+   callapi();
+  }
+
   return (
    <BrowserRouter>
     <Routes>
       <Route path='/' element={<Form/>}/>
       <Route path='login' element={<Login/>}/>
     </Routes>  
-   </BrowserRouter>  
+   </BrowserRouter>
+    
   );
 }
 
