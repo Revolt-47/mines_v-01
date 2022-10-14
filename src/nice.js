7215a2d91c693576b4db55d90c63175dc524bb1c
Revolt_47 <i201806@nu.edu.pk>
2022-10-14 18:11:33 +0500

    changed fetch to axios in nice

--- src/nice.js
+++ src/nice.js
@@ -1,13 +1,20 @@
 
 import React, {useEffect,useState} from 'react'
+const axios = require('axios').default
 
 export default function Nice(){
   
     const [backend,setbackend] = useState([{}])
 
-    useEffect(()=>{
-        fetch('/api').then(response=>response.json()).then(data => {setbackend(data)})
-    },[])
+    async function postData(url = '', data) {
+
+        axios.get('http://localhost:5000/api').then((response) => {
+          setbackend(response.data);
+          console.log(response.data);
+        });
+    }
+
+    postData('http://localhost:5000/api' , "salman")
 
     return(
         <div>
